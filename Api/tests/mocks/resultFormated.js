a161c1fa0cc8724b5461c0a330e5d65e105522ed
Airton <junynnhoh@gmail.com>
2022-07-18 18:44:43 -0300

    add json flow

--- Api/tests/mocks/resultFormated.js
+++ Api/tests/mocks/resultFormated.js
@@ -1,29 +1,29 @@
-const resultFormated = [
-  {
+const resultFormated = {
+  "challenge1": {
     "full_name": "takenet/library.data",
     "description": "Provides a simple abstraction for implementing the repository and unit of work patterns for data-enabled applications",
     "avatar_url": "https://avatars.githubusercontent.com/u/4369522?v=4"
   },
-  {
+  "challenge2": {
     "full_name": "takenet/library.logging",
     "description": "Provides a simple logging interface for applications and some basic implementations of this interface",
     "avatar_url": "https://avatars.githubusercontent.com/u/4369522?v=4"
   },
-  {
+  "challenge3": {
     "full_name": "takenet/CorrectNumber",
     "description": "This project have the single propose of creating a simple rest service to format telephone numbers",
     "avatar_url": "https://avatars.githubusercontent.com/u/4369522?v=4"
   },
-  {
+  "challenge4": {
     "full_name": "takenet/libphonenumber-csharp",
     "description": "Forking original c# port",
     "avatar_url": "https://avatars.githubusercontent.com/u/4369522?v=4"
   },
-  {
+  "challenge5": {
     "full_name": "takenet/Takenet.Radar",
     "description": "Radar de tecnologias takenet",
     "avatar_url": "https://avatars.githubusercontent.com/u/4369522?v=4"
   }
-]
+}
 
 module.exports = resultFormated;
--- Api/utils/url.js
+++ Api/utils/url.js
@@ -1,5 +1,8 @@
+const base = 'https://api.github.com/orgs/takenet/repos';
+const filters = '?per_page=5&sort=created&language=c23&direction=asc'
+
 const api = {
-  baseUrl: 'https://api.github.com/orgs/takenet/repos?per_page=5&sort=created&language=c23&direction=asc',
+  baseUrl: `${base}${filters}`,
   client_id: '',
   client_secret: '',
 }
