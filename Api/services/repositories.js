256162bed99e87d9d2484401fe6086035a1aa260
Airton <junynnhoh@gmail.com>
2022-07-18 15:15:24 -0300

    feat: convert array in object response

--- Api/services/repositories.js
+++ Api/services/repositories.js
@@ -8,13 +8,15 @@ const repositories = async () => {
 
   if (code !== 200) return verifyInvalidStatusCode(code);
 
-  const dataFilter = repos.data.map((repo, index) => ({
+  const data = repos.data.map((repo, index) => ({
     [`challenge${index + 1}`]: {
       full_name: repo.full_name,
       description: repo.description,
       avatar_url: repo.owner.avatar_url,
     }
   }));
+
+  const dataFilter = Object.assign({}, ...data);
 
   return { dataFilter, code };
 };
